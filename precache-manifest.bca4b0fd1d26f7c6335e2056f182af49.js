bd00669c83e3b4f3cc3e2e7fd1c3e46e60848483
Vinicius David <vinicius.davidf@gmail.com>
2020-06-21 14:05:29 -0300

    Updates

---
+++ precache-manifest.bca4b0fd1d26f7c6335e2056f182af49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52620cb0683de3241524959b9b399b07",
+    "url": "/neuralnetgenerator/index.html"
+  },
+  {
+    "revision": "0a071c1f341a2d47b246",
+    "url": "/neuralnetgenerator/static/js/2.d5bd805e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/neuralnetgenerator/static/js/2.d5bd805e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0c2dc1d65ac5505a74",
+    "url": "/neuralnetgenerator/static/js/main.b8fe372b.chunk.js"
+  },
+  {
+    "revision": "fbc351e92a4db8f476fb",
+    "url": "/neuralnetgenerator/static/js/runtime-main.75d7580b.js"
+  },
+  {
+    "revision": "27d85f5be55a5d5b0f66366280b433de",
+    "url": "/neuralnetgenerator/static/media/background.27d85f5b.png"
+  }
+]);
